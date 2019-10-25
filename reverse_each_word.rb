02c14aea3f54850b49630045fd4ed400d996fd9c
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-25 15:24:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word (str_arg)
   str_arg.each do {|statement| 
     split_statement = statement.split
-      split_statement.each do
+      split_statement.each do {|words|
+        rev_word = words.reverse 
+    end
+    sentence = rev_word.collect
   end 
 end 
